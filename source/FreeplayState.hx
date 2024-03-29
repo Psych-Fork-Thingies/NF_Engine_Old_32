ceec9f2448600a3db5b73dbbec04462da9b38341
Prohack101010 <miracberaber@gmail.com>
2024-03-29 20:38:39 +0300

    Add files via upload

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -300,6 +300,7 @@ class FreeplayState extends MusicBeatState
 		if (curSelected >= songs.length) curSelected = 0;
 		bg.color = songs[curSelected].color;
 		intendedColor = bg.color;
+		lerpSelected = curSelected;
 
 		curDifficulty = Math.round(Math.max(0, CoolUtil.defaultDifficulties.indexOf(lastDifficultyName)));
 
@@ -1076,8 +1077,8 @@ class FreeplayState extends MusicBeatState
 		curDifficulty += change;
 
 		if (curDifficulty < 0)
-			curDifficulty = curDifficulty.list.length-1;
-		if (curDifficulty >= curDifficulty.list.length)
+			curDifficulty = CoolUtil.difficulties.length-1;
+		if (curDifficulty >= CoolUtil.difficulties.length)
 			curDifficulty = 0;
 
 		#if !switch
@@ -1085,13 +1086,15 @@ class FreeplayState extends MusicBeatState
 		intendedRating = Highscore.getRating(songs[curSelected].songName, curDifficulty);
 		#end
 
-		lastDifficultyName = curDifficulty.getString(curDifficulty);
-		if (curDifficulty.list.length > 1)
+		lastDifficultyName = CoolUtil.difficulties.getString(curDifficulty);
+		if (CoolUtil.difficulties.length > 1)
 			diffText.text = '< ' + lastDifficultyName.toUpperCase() + ' >';
 		else
 			diffText.text = lastDifficultyName.toUpperCase();
 
 		positionHighscore();
+		missingText.visible = false;
+		missingTextBG.visible = false;
 	}
 
 	function changeSelection(change:Int = 0, playSound:Bool = true)
